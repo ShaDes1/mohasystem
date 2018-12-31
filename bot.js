c39c3f85f784618e5e1f106b636e515ff33a476a
ShaDes1 <46229960+ShaDes1@users.noreply.github.com>
2018-12-31 23:01:23 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,42 +27,4 @@ if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('
 
 
 
-const Discord =require('discord. js');
-const bot = new Discord.Client();
-
-bot.on('ready', () => {
-	console.log("Turning on Bot")
-	bot.user.setActivity("New Users", {
-		type: 2});
-});
-
-bot.on('guildMemberAdd', member => {
-	let channel = member.guild.channels.find('name', 'obx');
-	let memberavatar = member.user.avatar
-	if (!channel) return;
-	let embed = new Discord.RichEmbed()
-	.setcolor('RANDOM')
-	.setThumbnail(memberavatar)
-	.addField(':busts_in_silhouette: | name : ', `${member}`)
-	.addField(':microphone:  | Welcome!',` **Wellllcoome To ___OBx Shop____  :revolving_hearts: :gift_heart: ** , ${member}`)
-	.addField(':id: | user :', "**[" + `${member.id}` + "]**")
-	.addField(':family_mwgb: | Your the member',`${member.guild.memberCount}`)
-	.addField("Name", `<@` + `${member.id}` + `>`, true)
-	.addField('Server', `${member.guild.name}`, true)
-	.setFooter(`**${member.guild.name}**`)
-	.setTimestamp()
-	
-	channel.sendEmbed(embed);
-});
-
-bot.on('guildMemberAdd', member => {
-	
-	console.log(`${member}`, "has joined" + `${message.guild.name}`)
-	
-});
-
-
-
-
-
 client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
