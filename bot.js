25152c1c40236085e2e78c71c94a8a211d9d8275
ShaDes1 <46229960+ShaDes1@users.noreply.github.com>
2018-12-31 12:50:19 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,31 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+var prefix = "$";
 
+
+
+
+
+    client.on('message', message => {
+        var prefix = "A";
+        if (message.author.bot) return;
+        if (!message.content.startsWith(prefix)) return;
+      
+        let command = message.content.split(" ")[0];
+        command = command.slice(prefix.length);
+      
+      
+      let args = message.content.split(" ").slice(1);
+      let x = args.join(" ")
+        if(message.content.startsWith(prefix + 'S')) {
+            message.channel.send(''+b);
+                message.delete(999)
+        }
+        
+       
+      });
+
+
+
+
+client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
